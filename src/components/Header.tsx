0196c0f4a2eb909127dd6841583b5fcb2401657b
Vinay Maheshwari <vinaymaheshwari35@gmail.com>
2025-09-09 16:48:39 +0530

    ADD: crm apis

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -10,7 +10,7 @@ const Header = () => {
             {/* <div className="bg-gradient-blue p-2 rounded-lg">
               <Plane className="w-6 h-6 text-white" />
             </div> */}
-            <h1 className="text-2xl font-bold text-foreground">
+            <h1 className="text-xl md:text-2xl font-bold text-foreground">
               Autrova<span className="text-blue-600">.com</span>
             </h1>
           </div>
